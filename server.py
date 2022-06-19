6515087c818ece62ef3f8901ce2622fb22b141e3
Delta.v <tmm.michalski@gmail.com>
2022-06-19 17:58:18 +0200

    feat: create basic server

---
+++ server.py
@@ -0,0 +1,43 @@
+import socket
+from _thread import start_new_thread
+import sys
+
+server = "192.168.3.7"
+port = 5555
+
+# socket(domain, type)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    print(e)
+
+s.listen(2)
+print("Server start: waiting for a connection...")
+
+
+def threaded_client(conn):
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+                print("Sending:", reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
+
+    start_new_thread(threaded_client, (conn, ))
